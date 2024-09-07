04e60b8cd8546aeab54141cffe239dc0a08e42c6
Rajashree Behera <rajashree1119@gmail.com>
2024-09-07 17:02:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Board from './TicTacToeGame/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TIC-TAC-TOE </h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
